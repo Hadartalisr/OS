8f76ae455aef8802c771a14b96a6f4444eb1dc1f
hadartalisr <hadar.tal.isr@gmail.com>
2020-12-11 12:54:24 +0200

    working on radix tree functions

--- hw3/MessageSlot/message_sender.c
+++ hw3/MessageSlot/message_sender.c
@@ -1,4 +1,4 @@
-#include "chardev.h"    
+#include "message_slot.h"    
 
 #include <fcntl.h>      /* open */ 
 #include <unistd.h>     /* exit */
@@ -8,24 +8,6 @@
 
 int main()
 {
-  int file_desc;
-  int ret_val;
-
-  file_desc = open( "/dev/"DEVICE_FILE_NAME, O_RDWR );
-  if( file_desc < 0 ) 
-  {
-    printf ("Can't open device file: %s\n", 
-            DEVICE_FILE_NAME);
-    exit(-1);
-  }
-
-  ret_val = ioctl( file_desc, IOCTL_SET_ENC, 0);
-  ret_val = write( file_desc, "Hello", 5);
-  ret_val = read(  file_desc, NULL, 100 );
-  ret_val = ioctl( file_desc, IOCTL_SET_ENC, 1);
-  ret_val = write( file_desc, "Hello", 5);
-  ret_val = read(  file_desc, NULL, 100 );
- 
-  close(file_desc); 
+   
   return 0;
 }
--- hw3/MessageSlot/message_slot.c
+++ hw3/MessageSlot/message_slot.c
@@ -13,6 +13,7 @@
 #include <linux/uaccess.h>  /* for get_user and put_user */
 #include <linux/string.h>   /* for memset. NOTE - not string.h!*/
 #include <linux/radix-tree.h> /* the kernel has this data structure */
+#include <linux/slab.h> /* for kmalloc */
 MODULE_LICENSE("GPL");
 #include "message_slot.h"
 #define DEVICE_RANGE_NAME "message_slot"
@@ -36,7 +37,19 @@ typedef struct MESSAGE
 // used to prevent concurent access into the same device
 static int dev_open_flag = 0;
 static struct message_slot_info device_info;
+static struct radix_tree_root *minors[256]; // there can be at most 256 different message slots
 
+
+//================== Radix Tree FUNCTIONS ===========================
+
+int allocate_new_radix_tree(int minor_num){
+  if(minor_num < 0 || minor_num >= 256){
+    return SUCCESS;
+  }
+  minors[minor_num] = kmalloc(sizeof(struct radix_tree_root),GFP_KERNEL);
+  INIT_RADIX_TREE(minors[minor_num],GFP_KERNEL);
+  return SUCCESS;
+}
 
 
 
@@ -46,6 +59,10 @@ static int device_open( struct inode* inode,
                         struct file*  file )
 {
   unsigned long flags; // for spinlock
+  file_config* fc;
+
+
+  // spin_unlock
   printk("Invoking device_open(%p)\n", file);
   // We don't want to talk to two processes at the same time
   spin_lock_irqsave(&device_info.lock, flags);
@@ -58,8 +75,11 @@ static int device_open( struct inode* inode,
   spin_unlock_irqrestore(&device_info.lock, flags);
 
   
-  // allcate normal kernel ram 
-  file_config* fileConfig = kmalloc(sizeof(file_config),GFP_KERNEL); 
+  // allocate normal kernel ram 
+  fc = kmalloc(sizeof(file_config),GFP_KERNEL); 
+  fc->channel_id = 0;
+  fc->minor_num =  iminor(inode); // as was told in the recitation - 
+  // we can get the minor num from the inode.
 
 
 
@@ -121,15 +141,13 @@ static long device_ioctl( struct   file* file,
                           unsigned long  ioctl_param )
 {
   // Make sure that the CMD and the param (channel_id) are valid 
-  if((MSG_SLOT_CHANNEL |= ioctl_command_id ) || (ioctl_param <= 0) )
+  if((MSG_SLOT_CHANNEL != ioctl_command_id ) || (ioctl_param <= 0) )
   {
     return -EINVAL;
   }
 
-  file_co
   // Get the parameter given to ioctl by the process
   printk( "Invoking ioctl: setting MSG_SLOT_CHANNEL to %ld\n", ioctl_param );
-  channel = ioctl_param;
 
   return SUCCESS;
 }
@@ -169,15 +187,14 @@ static int __init simple_init(void)
   // Negative values signify an error
   if( rc < 0 )
   {
-    printk( KERN_ALERT "%s registraion failed for  %d\n",
-                       DEVICE_FILE_NAME, MAJOR_NUM );
+    printk( KERN_ALERT "%s registraion failed for  %d\n", DEVICE_RANGE_NAME, MAJOR_NUM );
     return rc;
   }
 
   printk( "Registeration is successful. ");
   printk( "If you want to talk to the device driver,\n" );
   printk( "you have to create a device file:\n" );
-  printk( "mknod /dev/%s c %d MINOR_NUM\n", DEVICE_FILE_NAME, MAJOR_NUM );
+  printk( "mknod /dev/%s c %d MINOR_NUM\n", DEVICE_RANGE_NAME, MAJOR_NUM );
   printk( "You can echo/cat to/from the device file.\n" );
   printk( "Dont forget to rm the device file and "
           "rmmod when you're done\n" );
