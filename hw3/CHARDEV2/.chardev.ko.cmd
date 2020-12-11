f4efc6d9cb3774db54ad0f6694639bc0d59cccb6
hadartalisr <hadar.tal.isr@gmail.com>
2020-12-11 11:18:25 +0200

    working

---
+++ hw3/CHARDEV2/.chardev.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/student/OS/hw3/CHARDEV2/chardev.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/student/OS/hw3/CHARDEV2/chardev.ko /home/student/OS/hw3/CHARDEV2/chardev.o /home/student/OS/hw3/CHARDEV2/chardev.mod.o ;  true
---
+++ hw3/CHARDEV2/.tmp_versions/chardev.mod
@@ -0,0 +1,3 @@
+/home/student/OS/hw3/CHARDEV2/chardev.ko
+/home/student/OS/hw3/CHARDEV2/chardev.o
+
