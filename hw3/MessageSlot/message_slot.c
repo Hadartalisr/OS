8ca95ae9df320ca3050d089cc5b077cc6d9ce173
hadartalisr <hadar.tal.isr@gmail.com>
2020-12-11 12:22:48 +0200

    working

--- hw3/MessageSlot/message_slot.c
+++ hw3/MessageSlot/message_slot.c
@@ -7,33 +7,39 @@
 #undef MODULE
 #define MODULE
 
-
 #include <linux/kernel.h>   /* We're doing kernel work */
 #include <linux/module.h>   /* Specifically, a module */
 #include <linux/fs.h>       /* for register_chrdev */
 #include <linux/uaccess.h>  /* for get_user and put_user */
 #include <linux/string.h>   /* for memset. NOTE - not string.h!*/
-
+#include <linux/radix-tree.h> /* the kernel has this data structure */
 MODULE_LICENSE("GPL");
+#include "message_slot.h"
+#define DEVICE_RANGE_NAME "message_slot"
 
-//Our custom definitions of IOCTL operations
-#include "chardev.h"
-
-struct chardev_info
+struct message_slot_info
 {
   spinlock_t lock;
 };
 
+typedef struct FILE_CONFIG{
+  int minor_num;
+  unsigned long channel_id ; 
+} file_config;
+
+typedef struct MESSAGE
+{
+  char buffer[BUF_LEN]; // the messfage buffer
+  int size; // the size of the message
+} message;
+
 // used to prevent concurent access into the same device
 static int dev_open_flag = 0;
+static struct message_slot_info device_info;
 
-static struct chardev_info device_info;
 
-// The message the device will give when asked
-static char the_message[BUF_LEN];
 
-//Do we need to encrypt?
-static int encryption_flag = 0;
+
 
 //================== DEVICE FUNCTIONS ===========================
 static int device_open( struct inode* inode,
@@ -41,7 +47,6 @@ static int device_open( struct inode* inode,
 {
   unsigned long flags; // for spinlock
   printk("Invoking device_open(%p)\n", file);
-
   // We don't want to talk to two processes at the same time
   spin_lock_irqsave(&device_info.lock, flags);
   if( 1 == dev_open_flag )
@@ -49,9 +54,17 @@ static int device_open( struct inode* inode,
     spin_unlock_irqrestore(&device_info.lock, flags);
     return -EBUSY;
   }
-
   ++dev_open_flag;
   spin_unlock_irqrestore(&device_info.lock, flags);
+
+  
+  // allcate normal kernel ram 
+  file_config* fileConfig = kmalloc(sizeof(file_config),GFP_KERNEL); 
+
+
+
+
+
   return SUCCESS;
 }
 
@@ -77,11 +90,7 @@ static ssize_t device_read( struct file* file,
                             size_t       length,
                             loff_t*      offset )
 {
-  // read doesnt really do anything (for now)
-  printk( "Invocing device_read(%p,%ld) - "
-          "operation not supported yet\n"
-          "(last written - %s)\n",
-          file, length, the_message );
+
   //invalid argument error
   return -EINVAL;
 }
@@ -98,9 +107,8 @@ static ssize_t device_write( struct file*       file,
   printk("Invoking device_write(%p,%ld)\n", file, length);
   for( i = 0; i < length && i < BUF_LEN; ++i )
   {
-    get_user(the_message[i], &buffer[i]);
-    if( 1 == encryption_flag )
-      the_message[i] += 1;
+     // get_user(the_message[i], &buffer[i]);
+
   }
  
   // return the number of input characters used
@@ -112,17 +120,24 @@ static long device_ioctl( struct   file* file,
                           unsigned int   ioctl_command_id,
                           unsigned long  ioctl_param )
 {
-  // Switch according to the ioctl called
-  if( IOCTL_SET_ENC == ioctl_command_id )
+  // Make sure that the CMD and the param (channel_id) are valid 
+  if((MSG_SLOT_CHANNEL |= ioctl_command_id ) || (ioctl_param <= 0) )
   {
-    // Get the parameter given to ioctl by the process
-    printk( "Invoking ioctl: setting encryption "
-            "flag to %ld\n", ioctl_param );
-    encryption_flag = ioctl_param;
+    return -EINVAL;
   }
+
+  file_co
+  // Get the parameter given to ioctl by the process
+  printk( "Invoking ioctl: setting MSG_SLOT_CHANNEL to %ld\n", ioctl_param );
+  channel = ioctl_param;
 
   return SUCCESS;
 }
+
+
+
+
+
 
 //==================== DEVICE SETUP =============================
 
@@ -144,8 +159,9 @@ static int __init simple_init(void)
 {
   int rc = -1;
   // init dev struct
-  memset( &device_info, 0, sizeof(struct chardev_info) );
+  memset( &device_info, 0, sizeof(struct message_slot_info) );
   spin_lock_init( &device_info.lock );
+
 
   // Register driver capabilities. Obtain major num
   rc = register_chrdev( MAJOR_NUM, DEVICE_RANGE_NAME, &Fops );
@@ -161,7 +177,7 @@ static int __init simple_init(void)
   printk( "Registeration is successful. ");
   printk( "If you want to talk to the device driver,\n" );
   printk( "you have to create a device file:\n" );
-  printk( "mknod /dev/%s c %d 0\n", DEVICE_FILE_NAME, MAJOR_NUM );
+  printk( "mknod /dev/%s c %d MINOR_NUM\n", DEVICE_FILE_NAME, MAJOR_NUM );
   printk( "You can echo/cat to/from the device file.\n" );
   printk( "Dont forget to rm the device file and "
           "rmmod when you're done\n" );
@@ -172,10 +188,11 @@ static int __init simple_init(void)
 //---------------------------------------------------------------
 static void __exit simple_cleanup(void)
 {
-  // Unregister the device
-  // Should always succeed
+  // Unregister the device - Should always succeed
   unregister_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME);
 }
+
+
 
 //---------------------------------------------------------------
 module_init(simple_init);
--- hw3/MessageSlot/message_slot.h
+++ hw3/MessageSlot/message_slot.h
@@ -1,5 +1,5 @@
-#ifndef CHARDEV_H
-#define CHARDEV_H
+#ifndef MESSAGE_SLOT_H
+#define MESSAGE_SLOT_H
 
 #include <linux/ioctl.h>
 
@@ -7,15 +7,12 @@
 // We don't rely on dynamic registration
 // any more. We want ioctls to know this
 // number at compile time.
-//#define MAJOR_NUM 244
+
 #define MAJOR_NUM 240
 
 // Set the message of the device driver
-#define IOCTL_SET_ENC _IOW(MAJOR_NUM, 0, unsigned long)
-
-#define DEVICE_RANGE_NAME "char_dev"
-#define BUF_LEN 80
-#define DEVICE_FILE_NAME "simple_char_dev"
+#define MSG_SLOT_CHANNEL _IOW(MAJOR_NUM, 0, unsigned long) // IOCTL_SET_ENC
+#define BUF_LEN 128
 #define SUCCESS 0
 
 #endif
