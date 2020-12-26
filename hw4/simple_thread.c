ecb7ca4199fcff387e1aeb4298c9ed2cee253160
student <hadar.tal.isr@gmail.com>
2020-12-26 11:36:55 +0200

    start hw4

---
+++ hw4/simple_thread.c
@@ -0,0 +1,22 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void* thread_func(void* thread_params){
+    printf("i'm thread #%lu\n", pthread_self());
+    printf("I've got %s\n", (char*)thread_params);
+    pthread_exit((void*)5);
+}
+
+int main(int argc, char* argv[]){
+    printf("main thread #%lu\n", pthread_self());  
+    pthread_t thread_id ; 
+    void* status; 
+    pthread_create(&thread_id, NULL, thread_func, (void*)("hello !"));
+    
+    pthread_join(thread_id, &status);
+    printf("main - %d\n",(int*)status);
+
+}
